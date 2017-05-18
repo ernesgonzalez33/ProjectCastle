99698202adc51f1ea0d4857198dbabf52f627848
Ernesto Gonzalez <ernesgonzalez.33.egc@icloud.com>
2017-05-18 17:29:24 +0200

    Iniciado el TiledMap

--- core/src/com/projectcastle/game/Map.java
+++ core/src/com/projectcastle/game/Map.java
@@ -1,10 +1,12 @@
 package com.projectcastle.game;
 
+import com.badlogic.gdx.maps.tiled.TiledMap;
+
 /**
  * Created by ernestogonzalezchacon on 12/5/17.
  */
 
-public class Map {
+public class Map extends TiledMap {
 
     public static final String TAG = Map.class.getName();
 
