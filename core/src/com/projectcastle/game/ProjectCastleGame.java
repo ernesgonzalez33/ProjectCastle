91a978809299a779d784d50b9f22433b4ebf97fd
Ernesto Gonzalez <ernesgonzalez.33.egc@icloud.com>
2017-08-24 18:02:48 +0200

    Puesto el inputprocesor en la clase Game

--- core/src/com/projectcastle/game/ProjectCastleGame.java
+++ core/src/com/projectcastle/game/ProjectCastleGame.java
@@ -3,10 +3,12 @@ package com.projectcastle.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
@@ -14,13 +16,15 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.projectcastle.game.util.Constants;
 import com.projectcastle.game.util.MapLoader;
+import com.projectcastle.game.util.OrthogonalTiledMapRendererWithSprites;
 
-public class ProjectCastleGame extends Game {
+class ProjectCastleGame extends Game implements InputProcessor {
 	SpriteBatch batch;
 	TiledMap map;
 	OrthographicCamera camera;
 	OrthogonalTiledMapRenderer renderer;
 	Viewport viewport;
+	MapLayer objectLayer;
 
 	@Override
 	public void create () {
@@ -30,6 +34,7 @@ public class ProjectCastleGame extends Game {
 		camera.setToOrtho(false, 20, 20);
 		camera.update();
 		renderer = new OrthogonalTiledMapRenderer(map, Constants.UNIT_SCALE);
+		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
