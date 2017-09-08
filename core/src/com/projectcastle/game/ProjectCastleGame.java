fbc14a58358274a7c00e413e9b3b5683e6d909e5
Ernesto Gonzalez <ernesgonzalez.33.egc@icloud.com>
2017-09-08 13:29:28 +0200

    Borrada clase que no servía para nada

--- core/src/com/projectcastle/game/ProjectCastleGame.java
+++ core/src/com/projectcastle/game/ProjectCastleGame.java
@@ -1,24 +1,18 @@
 package com.projectcastle.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.projectcastle.game.util.Constants;
 import com.projectcastle.game.util.MapLoader;
-import com.projectcastle.game.util.OrthogonalTiledMapRendererWithSprites;
 
 public class ProjectCastleGame extends Game {
 
