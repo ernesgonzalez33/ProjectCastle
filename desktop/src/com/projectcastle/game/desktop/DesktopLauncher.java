8576daa0bb60792ca6e4f1516ff557c46bf872bc
Ernesto Gonzalez <ernesgonzalez.33.egc@icloud.com>
2017-09-11 19:12:49 +0200

    Añadidos los tamaños de ancho y alto

--- desktop/src/com/projectcastle/game/desktop/DesktopLauncher.java
+++ desktop/src/com/projectcastle/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.projectcastle.game.ProjectCastleGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "ProjectCastle";
+		config.width = 320;
+		config.height = 320;
 		new LwjglApplication(new ProjectCastleGame(), config);
 	}
 }
