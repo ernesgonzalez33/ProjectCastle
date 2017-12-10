4b565f39b777e06d7fe186dd0a1d6b71bd623bb7
Ernesto Gonzalez <ernesgonzalez.33.egc@icloud.com>
2017-12-10 19:22:34 +0100

    Added ActionMenu as a class. Currently investigating if it works that way

--- core/src/com/projectcastle/game/ProjectCastleGame.java
+++ core/src/com/projectcastle/game/ProjectCastleGame.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.projectcastle.game.screens.ActionMenu;
 import com.projectcastle.game.screens.TestMapScreen;
 
 public class ProjectCastleGame extends Game {
@@ -15,14 +16,17 @@ public class ProjectCastleGame extends Game {
 	public SpriteBatch batch;
 	public OrthogonalTiledMapRenderer tiledMapRenderer;
 	public AssetManager manager;
+	public ActionMenu actionMenu;
 
 	@Override
 	public void create() {
 
 		manager = new AssetManager();
 		batch = new SpriteBatch();
+		actionMenu = new ActionMenu();
 		this.setScreen(new TestMapScreen(this));
 		fpsLogger = new FPSLogger();
+
 
 	}
 
