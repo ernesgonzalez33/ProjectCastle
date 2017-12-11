bad145d4169128fbbdb546404a1d2291f0a239d7
Ernesto Gonzalez <ernesgonzalez.33.egc@icloud.com>
2017-12-11 21:59:35 +0100

    Created class ActionMenu. Need a skin

---
+++ core/src/com/projectcastle/game/screens/ActionMenu.java
@@ -0,0 +1,16 @@
+package com.projectcastle.game.screens;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
+/**
+ * Created by ernestogonzalezchacon on 3/12/17.
+ */
+
+public class ActionMenu extends Table {
+
+//    Label move = new Label("Move", );
+//    Label attack;
+
+    //TODO: Meter skin flat en mi juego
+}
