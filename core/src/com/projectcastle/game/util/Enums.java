cf16dc10baf731165f20814afd8af65a342578bd
Ernesto Gonzalez <ernesgonzalez.33.egc@icloud.com>
2018-02-01 18:54:32 +0100

    Enums class with HeroState added

---
+++ core/src/com/projectcastle/game/util/Enums.java
@@ -0,0 +1,14 @@
+package com.projectcastle.game.util;
+
+/**
+ * Created by Ernesto Gonzalez on 1/2/18.
+ * Kind of class: com.projectcastle.game.util
+ */
+
+public class Enums {
+
+    public enum HeroState {
+        MOVING, ATTACKING, IDLE
+    }
+
+}
