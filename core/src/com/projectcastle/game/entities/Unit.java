48319ebc3d02ac32048c8fa9549694dcc691661d
Ernesto Gonzalez <ernesgonzalez.33.egc@icloud.com>
2018-03-22 23:09:21 +0100

    Testing Steerable in Unit. Still a lot of work to do

--- core/src/com/projectcastle/game/entities/Unit.java
+++ core/src/com/projectcastle/game/entities/Unit.java
@@ -1,5 +1,8 @@
 package com.projectcastle.game.entities;
 
+import com.badlogic.gdx.ai.steer.Steerable;
+import com.badlogic.gdx.ai.steer.SteeringBehavior;
+import com.badlogic.gdx.ai.utils.Location;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -19,7 +22,7 @@ import java.util.ArrayList;
  * Kind of class: ${PACKAGE_NAME}
  */
 
-public class Unit extends Actor {
+public class Unit extends Actor implements Steerable<Vector2> {
 
     public final static String TAG = Unit.class.getName();
     private int attack;
@@ -31,6 +34,21 @@ public class Unit extends Actor {
     private int moveLimit;
     private TemplateScreen screen;
     private ArrayList<Vector2> canMovePositions;
+    Vector2 position;
+    Vector2 linearVelocity;
+    float angularVelocity;
+    float boundingRadius;
+    boolean tagged;
+
+    float maxLinearSpeed = 100;
+    float maxLinearAcceleration = 200;
+    float maxAngularSpeed = 5;
+    float maxAngularAcceleration = 10;
+
+    boolean independentFacing;
+
+    SteeringBehavior<Vector2> steeringBehavior;
+
 
     Unit(float positionX, float positionY, int attack, int defense, String name, final int health, TextureRegion region, final ActionMenu actionMenu, int moveLimit, TemplateScreen screen) {
 
@@ -48,6 +66,7 @@ public class Unit extends Actor {
         this.moveLimit = moveLimit;
         this.screen = screen;
         this.canMovePositions = new ArrayList<Vector2>();
+        this.independentFacing = true;
 
     }
 
@@ -81,6 +100,12 @@ public class Unit extends Actor {
         } else {
             attackingUnit.setAttack(attackingUnit.getAttack() + 2);
         }
+    }
+
+    @Override
+    public void setPosition(float x, float y) {
+        super.setPosition(x, y);
+        this.position = new Vector2(x, y);
     }
 
     public String getName() { return name; }
@@ -214,6 +239,203 @@ public class Unit extends Actor {
             }
         }
         return false;
+    }
+
+
+    /**
+     * Returns the vector indicating the linear velocity of this Steerable.
+     */
+    @Override
+    public Vector2 getLinearVelocity() {
+        return null;
+    }
+
+    /**
+     * Returns the float value indicating the the angular velocity in radians of this Steerable.
+     */
+    @Override
+    public float getAngularVelocity() {
+        return 0;
+    }
+
+    /**
+     * Returns the bounding radius of this Steerable.
+     */
+    @Override
+    public float getBoundingRadius() {
+        return 0;
+    }
+
+    /**
+     * Returns {@code true} if this Steerable is tagged; {@code false} otherwise.
+     */
+    @Override
+    public boolean isTagged() {
+        return false;
+    }
+
+    /**
+     * Tag/untag this Steerable. This is a generic flag utilized in a variety of ways.
+     *
+     * @param tagged the boolean value to set
+     */
+    @Override
+    public void setTagged(boolean tagged) {
+
+    }
+
+    /**
+     * Returns the threshold below which the linear speed can be considered zero. It must be a small positive value near to zero.
+     * Usually it is used to avoid updating the orientation when the velocity vector has a negligible length.
+     */
+    @Override
+    public float getZeroLinearSpeedThreshold() {
+        return 0;
+    }
+
+    /**
+     * Sets the threshold below which the linear speed can be considered zero. It must be a small positive value near to zero.
+     * Usually it is used to avoid updating the orientation when the velocity vector has a negligible length.
+     *
+     * @param value
+     */
+    @Override
+    public void setZeroLinearSpeedThreshold(float value) {
+
+    }
+
+    /**
+     * Returns the maximum linear speed.
+     */
+    @Override
+    public float getMaxLinearSpeed() {
+        return 0;
+    }
+
+    /**
+     * Sets the maximum linear speed.
+     *
+     * @param maxLinearSpeed
+     */
+    @Override
+    public void setMaxLinearSpeed(float maxLinearSpeed) {
+
+    }
+
+    /**
+     * Returns the maximum linear acceleration.
+     */
+    @Override
+    public float getMaxLinearAcceleration() {
+        return 0;
+    }
+
+    /**
+     * Sets the maximum linear acceleration.
+     *
+     * @param maxLinearAcceleration
+     */
+    @Override
+    public void setMaxLinearAcceleration(float maxLinearAcceleration) {
+
+    }
+
+    /**
+     * Returns the maximum angular speed.
+     */
+    @Override
+    public float getMaxAngularSpeed() {
+        return 0;
+    }
+
+    /**
+     * Sets the maximum angular speed.
+     *
+     * @param maxAngularSpeed
+     */
+    @Override
+    public void setMaxAngularSpeed(float maxAngularSpeed) {
+
+    }
+
+    /**
+     * Returns the maximum angular acceleration.
+     */
+    @Override
+    public float getMaxAngularAcceleration() {
+        return 0;
+    }
+
+    /**
+     * Sets the maximum angular acceleration.
+     *
+     * @param maxAngularAcceleration
+     */
+    @Override
+    public void setMaxAngularAcceleration(float maxAngularAcceleration) {
+
+    }
+
+    /**
+     * Returns the vector indicating the position of this location.
+     */
+    @Override
+    public Vector2 getPosition() {
+        return null;
+    }
+
+    /**
+     * Returns the float value indicating the orientation of this location. The orientation is the angle in radians representing
+     * the direction that this location is facing.
+     */
+    @Override
+    public float getOrientation() {
+        return 0;
+    }
+
+    /**
+     * Sets the orientation of this location, i.e. the angle in radians representing the direction that this location is facing.
+     *
+     * @param orientation the orientation in radians
+     */
+    @Override
+    public void setOrientation(float orientation) {
+
+    }
+
+    /**
+     * Returns the angle in radians pointing along the specified vector.
+     *
+     * @param vector the vector
+     */
+    @Override
+    public float vectorToAngle(Vector2 vector) {
+        return 0;
+    }
+
+    /**
+     * Returns the unit vector in the direction of the specified angle expressed in radians.
+     *
+     * @param outVector the output vector.
+     * @param angle     the angle in radians.
+     * @return the output vector for chaining.
+     */
+    @Override
+    public Vector2 angleToVector(Vector2 outVector, float angle) {
+        return null;
+    }
+
+    /**
+     * Creates a new location.
+     * <p>
+     * This method is used internally to instantiate locations of the correct type parameter {@code T}. This technique keeps the API
+     * simple and makes the API easier to use with the GWT backend because avoids the use of reflection.
+     *
+     * @return the newly created location.
+     */
+    @Override
+    public Location<Vector2> newLocation() {
+        return null;
     }
 
 }
